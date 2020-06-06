18be6a08f914480842b1cb2b4c5dc3aa33cd3dc2
Lofipure <littlebanana@126.com>
2020-06-06 17:39:02 +0800

    final commit

--- pages/config/config.js
+++ pages/config/config.js
@@ -6,7 +6,9 @@ Page({
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
     var configs = wx.getStorageSync('configs');
-    this.setData({ configs: configs });
+    this.setData({
+      configs: configs
+    });
   },
   onReady: function () {
     // 页面渲染完成
@@ -29,7 +31,9 @@ Page({
       configs[id] = config;
     }
     config.state = e.detail.value;
-    this.setData({ configs: configs });
+    this.setData({
+      configs: configs
+    });
     wx.setStorageSync('configs', configs);
   },
   sliderChange: function (e) {
@@ -41,7 +45,9 @@ Page({
       configs[id] = config;
     }
     config.time = e.detail.value;
-    this.setData({ configs: configs });
+    this.setData({
+      configs: configs
+    });
     wx.setStorageSync('configs', configs);
   },
   radioChange: function (e) {
@@ -53,25 +59,31 @@ Page({
       configs[id] = config;
     }
     config.voice = e.detail.value;
-    this.setData({ configs: configs });
+    this.setData({
+      configs: configs
+    });
     wx.setStorageSync('configs', configs);
   },
-  inputChange: function(e) {
+  inputChange: function (e) {
     var configs = this.data.configs;
     configs.title = e.detail.value;
-    this.setData({configs: configs});
+    this.setData({
+      configs: configs
+    });
     wx.setStorageSync('configs', configs);
   },
-  configInputChage: function(e) {
+  configInputChage: function (e) {
     var id = e.target.id;
     var configs = this.data.configs;
     var config = configs[id];
-    if(!config) {
+    if (!config) {
       config = new Object();
       configs[id] = config;
     }
     config.time = e.detail.value;
-    this.setData({configs: configs});
+    this.setData({
+      configs: configs
+    });
     wx.setStorageSync('configs', configs);
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,122 +1,168 @@
 Page({
-  leftMove:0,
-  rightMove:0,
-  data:{
+  leftMove: 0,
+  rightMove: 0,
+  data: {
     // text:"这是一个页面"
     actionSheetHidden: true,
     actionSheetItems: [],
-    title:"",
-    desc:"",
-    voice:0,
-    leftTime:0,
-    rightTime:0,
+    title: "",
+    desc: "",
+    voice: 0,
+    leftTime: 0,
+    rightTime: 0,
     src: '/assets/sound/countdown.mp3',
     mainTitle: "",
     left: ">>    ",
-    right: "    <<"
+    right: "    <<",
+    sum: 0,
+    lp: 100,
+    rp: 100,
+    thatThat: undefined
   },
-  onLoad:function(options){
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
+    var that = this;
+    that.canvasRing = that.selectComponent("#canvasRing");
+    that.canvasRing.showCanvasRing();
+    that.canvasRing = that.selectComponent("#canvasRingRight");
+    that.canvasRing.showCanvasRing();
   },
-  onShow:function(){
+  onShow: function () {
     var configs = wx.getStorageSync('configs');
-    var actionSheetItems=[];
-    var first=true;
-    for(var i in configs){
-        var config = configs[i];
-        if(config.state){
-          if(first){
-            var desc = config.desc.replace(/@/g,config.time+"秒");
-            this.setData({title:config.name,desc:desc,leftTime:config.time,rightTime:config.time,voice:config.voice});
-            first=false;
-          }
-          actionSheetItems.push({name:config.name,id:config.id});
+    var actionSheetItems = [];
+    var first = true;
+    for (var i in configs) {
+      var config = configs[i];
+      if (config.state) {
+        if (first) {
+          var desc = config.desc.replace(/@/g, config.time + "秒");
+          this.setData({
+            title: config.name,
+            desc: desc,
+            leftTime: config.time,
+            rightTime: config.time,
+            voice: config.voice,
+            sum: config.time
+          });
+          first = false;
         }
+        actionSheetItems.push({
+          name: config.name,
+          id: config.id
+        });
+      }
     }
-    this.setData({mainTitle: configs.title});
-    this.setData({actionSheetItems:actionSheetItems});
+    this.setData({
+      mainTitle: configs.title
+    });
+    this.setData({
+      actionSheetItems: actionSheetItems
+    });
 
     // 页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
   },
-  actionSheetTap: function(e) {
+  actionSheetTap: function (e) {
     this.setData({
       actionSheetHidden: !this.data.actionSheetHidden
     })
   },
-  actionSheetChange: function(e) {
+  actionSheetChange: function (e) {
     this.setData({
       actionSheetHidden: !this.data.actionSheetHidden
     })
   },
-  bindItemTap:function(e){
+  bindItemTap: function (e) {
     this.leftStop();
     this.rightStop();
     var id = e.target.id;
     var configs = wx.getStorageSync('configs');
     var config = configs[id];
-    var desc = config.desc.replace(/@/g,config.time+"秒");
-    this.setData({title:config.name,desc:desc,actionSheetHidden:true,leftTime:config.time,rightTime:config.time,voice:config.voice});
+    var desc = config.desc.replace(/@/g, config.time + "秒");
+    this.setData({
+      title: config.name,
+      desc: desc,
+      actionSheetHidden: true,
+      leftTime: config.time,
+      rightTime: config.time,
+      voice: config.voice,
+      sum: config.time,
+      lp: 100,
+      rg: 100
+    });
+    page.canvasRing = page.selectComponent("#canvasRingRight");
+    page.canvasRing.showCanvasRing();
+    page.canvasRing = page.selectComponent("#canvasRing");
+    page.canvasRing.showCanvasRing();
   },
-  leftStop:function(){
+  leftStop: function () {
     clearInterval(this.leftInterval);
-    this.leftInterval=0;
+    this.leftInterval = 0;
     this.audioPause();
   },
-  rightStop:function(){
+  rightStop: function () {
     clearInterval(this.rightInterval);
-    this.rightInterval=0;
+    this.rightInterval = 0;
     this.audioPause();
   },
-  leftStart:function(){
+  leftStart: function () {
     this.rightStop();
-    if(this.leftInterval&&this.leftInterval!=0){
+    if (this.leftInterval && this.leftInterval != 0) {
       this.leftStop();
       return;
     }
 
     var page = this;
-    var leftInterval = setInterval(function(){
-      if(page.data.leftTime<=0){
-         page.leftStop();
-         return;
+    page.canvasRing = page.selectComponent("#canvasRing");
+    var leftInterval = setInterval(function () {
+      if (page.data.leftTime <= 0) {
+        page.leftStop();
+        return;
       }
-      if(page.data.leftTime<=page.data.voice){
+      if (page.data.leftTime <= page.data.voice) {
         page.audioPlay();
       }
-      page.setData({leftTime:page.data.leftTime-1});
-    },1000);
-    this.leftInterval=leftInterval;
-    
+      page.setData({
+        leftTime: page.data.leftTime - 1,
+        lp: (page.data.leftTime / page.data.sum).toFixed(2) * 100
+      });
+      page.canvasRing.showCanvasRing();
+    }, 1000);
+    this.leftInterval = leftInterval;
   },
-  rightStart:function(){
+  rightStart: function () {
     this.leftStop();
-    if(this.rightInterval&&this.rightInterval!=0){
+    if (this.rightInterval && this.rightInterval != 0) {
       this.rightStop();
       return;
     }
     var page = this;
-    var rightInterval = setInterval(function(){
-      if(page.data.rightTime<=0){
-         page.rightStop();
-         return;
+    page.canvasRing = page.selectComponent("#canvasRingRight");
+    var rightInterval = setInterval(function () {
+      if (page.data.rightTime <= 0) {
+        page.rightStop();
+        return;
       }
-      if(page.data.rightTime<=page.data.voice){
+      if (page.data.rightTime <= page.data.voice) {
         page.audioPlay();
       }
-      page.setData({rightTime:page.data.rightTime-1});
-    },1000);
-    this.rightInterval=rightInterval;
+      page.setData({
+        rightTime: page.data.rightTime - 1,
+        rp: (page.data.rightTime / page.data.sum).toFixed(2) * 100
+      });
+      page.canvasRing.showCanvasRing();
+    }, 1000);
+    this.rightInterval = rightInterval;
   },
-   audioPlay: function () {
+  audioPlay: function () {
     this.setData({
       action: {
         method: 'play'
